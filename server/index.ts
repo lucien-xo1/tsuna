192c4b46a83560eb3a78f5109a1d6b9efe4f8631
lucien-xo1 <lucienecho.xo@gmail.com>
2025-09-08 00:08:29 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -20,9 +20,9 @@ app.use((req, res, next) => {
   res.on("finish", () => {
     const duration = Date.now() - start;
     if (path.startsWith("/api")) {
-      let logLine = ${req.method} ${path} ${res.statusCode} in ${duration}ms;
+      let logLine = `${req.method} ${path} ${res.statusCode} in ${duration}ms`;
       if (capturedJsonResponse) {
-        logLine +=  :: ${JSON.stringify(capturedJsonResponse)};
+        logLine += ` :: ${JSON.stringify(capturedJsonResponse)}`;
       }
 
       if (logLine.length > 80) {
@@ -66,6 +66,7 @@ app.use((req, res, next) => {
     host: "0.0.0.0",
     reusePort: true,
   }, () => {
-    log(serving on port ${port});
+    log(`serving on port ${port}`);
   });
 })();
+
