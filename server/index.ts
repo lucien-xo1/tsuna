72b9b15d32646431913dd7667d11a523931655ae
lucien-xo1 <lucienecho.xo@gmail.com>
2025-09-07 23:19:41 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -20,9 +20,9 @@ app.use((req, res, next) => {
   res.on("finish", () => {
     const duration = Date.now() - start;
     if (path.startsWith("/api")) {
-      let logLine = `${req.method} ${path} ${res.statusCode} in ${duration}ms`;
+      let logLine = ${req.method} ${path} ${res.statusCode} in ${duration}ms;
       if (capturedJsonResponse) {
-        logLine += ` :: ${JSON.stringify(capturedJsonResponse)}`;
+        logLine +=  :: ${JSON.stringify(capturedJsonResponse)};
       }
 
       if (logLine.length > 80) {
@@ -42,18 +42,30 @@ app.use((req, res, next) => {
   app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
     const status = err.status || err.statusCode || 500;
     const message = err.message || "Internal Server Error";
+
     res.status(status).json({ message });
     throw err;
   });
 
+  // importantly only setup vite in development and after
+  // setting up all the other routes so the catch-all route
+  // doesn't interfere with the other routes
   if (app.get("env") === "development") {
     await setupVite(app, server);
   } else {
     serveStatic(app);
   }
 
-  const port = parseInt(process.env.PORT || "8080", 10); // Cloud Run default is 8080
-  server.listen(port, "0.0.0.0", () => {
-    log(`🚀 serving on port ${port}`);
+  // ALWAYS serve the app on the port specified in the environment variable PORT
+  // Other ports are firewalled. Default to 5000 if not specified.
+  // this serves both the API and the client.
+  // It is the only port that is not firewalled.
+  const port = parseInt(process.env.PORT || '5000', 10);
+  server.listen({
+    port,
+    host: "0.0.0.0",
+    reusePort: true,
+  }, () => {
+    log(serving on port ${port});
   });
 })();
